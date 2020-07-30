ab73bb567f1fd32a3b9e84105f0fda59ff290a2e
Leon <yuanfeng0905@gmail.com>
2020-07-30 18:02:04 +0800

    init

--- cmd/drone-azoya-harbor/main.go
+++ cmd/drone-azoya-harbor/main.go
@@ -9,16 +9,23 @@ import (
 func main() {
 	var (
 		registry = "hub.azoyagroup.com"
-		email    = getenv("PLUGIN_EMAIL", "HEROKU_EMAIL")
-		key      = getenv("PLUGIN_API_KEY", "HEROKU_API_KEY")
+		username = os.Getenv("PLUGIN_USERNAME")
+		password = os.Getenv("PLUGIN_PASSWORD")
 	)
 
 	os.Setenv("PLUGIN_REGISTRY", registry)
 	os.Setenv("PLUGIN_REPO", path.Join(registry, "azoya", os.Getenv("DRONE_REPO_NAME")))
 
-	os.Setenv("DOCKER_PASSWORD", key)
-	os.Setenv("DOCKER_USERNAME", email)
-	os.Setenv("DOCKER_EMAIL", email)
+	// 如果未配置，默认使用运维账号权限
+	if username == "" {
+		username = "ops@haituncun.com"
+	}
+	if password == "" {
+		password = "Azoya#0422"
+	}
+
+	os.Setenv("DOCKER_USERNAME", username)
+	os.Setenv("DOCKER_PASSWORD", password)
 
 	cmd := exec.Command("drone-docker")
 	cmd.Stdout = os.Stdout
@@ -27,14 +34,4 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func getenv(key ...string) (s string) {
-	for _, k := range key {
-		s = os.Getenv(k)
-		if s != "" {
-			return
-		}
-	}
-	return
 }
